fe75222d2331bcf4cf7eb533a3b4586d6ccbd6d0
ShevHet <141827983+ShevHet@users.noreply.github.com>
2024-11-22 01:27:27 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var client = new HttpClient { BaseAddress = new Uri("https://localhost:5002/api/Directories/") };
+
+
+        // Создание корневой директории
+        var root = new { Name = "Root", ParentId = (int?)null };
+        var response = await client.PostAsJsonAsync("", root);
+        var createdRoot = await response.Content.ReadFromJsonAsync<Directory>();
+
+        Console.WriteLine($"Создана директория: {createdRoot?.Name} с Id: {createdRoot?.Id}");
+
+        // Создание дочерней директории
+        var child = new { Name = "Child", ParentId = createdRoot?.Id };
+        response = await client.PostAsJsonAsync("", child);
+        var createdChild = await response.Content.ReadFromJsonAsync<Directory>();
+
+        Console.WriteLine($"Создана дочерняя директория: {createdChild?.Name} с Id: {createdChild?.Id}");
+
+        // Получение всех директорий
+        var directories = await client.GetFromJsonAsync<List<Directory>>("https://localhost:5002/api/Directories/");
+
+        Console.WriteLine("Дерево директорий:");
+        if (directories != null)
+        {
+            foreach (var dir in directories)
+            {
+                Console.WriteLine($"{dir.Name} (Id: {dir.Id}, ParentId: {dir.ParentId})");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Список директорий пуст.");
+        }
+    }
+}
+
+public class Directory
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int? ParentId { get; set; }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("DirectoryTreeClient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DirectoryTreeClient")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7a9fd4cf-1f10-4782-afc9-d2cbb58ce3a4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
